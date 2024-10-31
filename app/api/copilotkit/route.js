5c598f42722d05bf8eefb8d1c214f86cc2eae004
mohammed <mohadany@hotmail.com>
2024-10-31 21:32:31 +0500

    minor changes

--- app/api/copilotkit/route.js
+++ app/api/copilotkit/route.js
@@ -5,7 +5,10 @@ import {
   } from '@copilotkit/runtime';
   import { NextRequest } from 'next/server';
   const { GoogleGenerativeAI } = require("@google/generative-ai");
-  const genAI = new GoogleGenerativeAI(process.env["GOOGLE_API_KEY"]);
+  // const genAI = new GoogleGenerativeAI(process.env["GOOGLE_API_KEY"]);
+    const genAI = new GoogleGenerativeAI("AIzaSyDaUCmSj37GGtFuPMox4hvTso_XADrSBoE");
+
+  
 
  const runtime = new CopilotRuntime();
   
@@ -39,3 +42,23 @@ export const POST = async (req) => {
     return new Response("Error handling request", { status: 500 });
   }
 };
+// CopilotRuntime,
+// OpenAIAdapter,
+// copilotRuntimeNextJSAppRouterEndpoint,
+// } from '@copilotkit/runtime';
+// import OpenAI from 'openai';
+// import { NextRequest } from 'next/server';
+
+// const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+// const serviceAdapter = new OpenAIAdapter({ openai });
+// const runtime = new CopilotRuntime();
+
+// export const POST = async (req) => {
+// const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
+//   runtime,
+//   serviceAdapter,
+//   endpoint: '/api/copilotkit',
+// });
+
+// return handleRequest(req);
+// };
